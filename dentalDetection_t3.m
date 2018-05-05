5a1f7d3dd1f4d0d38efe15af0c4a1c766bdceb52
unknown <ice_law@hotmail.com>
2018-05-05 23:00:34 +0800

    Finished identify cavity percentage

--- dentalDetection_t3.m
+++ dentalDetection_t3.m
@@ -3,30 +3,31 @@ close all
 clear all
 
 %Read image files
-I = imread('t3.jpg');
+I = imread('case2.jpg');
 
 %Convert images to grayscale
 gray = rgb2gray(I);
 
 %Show image and histogram
 figure;
-subplot(2,1,1), imshow(I), title('t1');
-subplot(2,1,2), imshow(gray), title('t1');
+subplot(3,2,1), imshow(I), title('Original');
+subplot(3,2,3), imshow(gray), title('Grayscale');
 
 %Filter image using median filtering
 fil = medfilt2(gray,[3 3]);
-figure;
-imshow(fil), title('Median filter');
-%fil2 = filter2(fspecial('average',3),gray)/255;
-%subplot(1,2,2),imshow(fil2), title('average filter');
+subplot(3,2,4), imshow(fil), title('Median filter');
 
-%Sharpen the image 
-figure;
+%Sharpen the image
 sharp = imsharpen(fil);
-subplot(1,2,1), imshow(fil), title('Sharpen image');
-subplot(1,2,2), imhist(fil), title('Histogram t1');
-%figure, imshow(imsharpen(fil2)),title('Sharpen ave');
+subplot(3,2,5), imshow(fil), title('Sharpen image');
+subplot(3,2,6), imhist(fil), title('Histogram of Sharpen img');
 
 %Convert image to binary
-bin = roicolor(sharp, 60, 255);% all pixels in (120, 255) will be 1 and white
+bin = roicolor(sharp, 65, 255);% all pixels in (65,255) will be 1 and white
 figure, imshow(bin), title('Binary image'); % the rest is 0 which is black
+
+%calculate percentage of cavity
+white = nnz(bin)                        %total white pixels (non cavity)
+black = numel(bin)-white                %total black pixels (cavity)
+blackPercent = (black/numel(bin))*100   %percentage of black pixels
+
